307206166dccf7677e1a4cdfe5319fc252056169
Cam Crosbie <cam@camcrosbie.com>
2021-02-13 08:26:13 -0500

    updates re Walleye Store

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -2,9 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'walleye-root',
-  templateUrl: 'app.component.html'
+  templateUrl: 'app.component.html',
 })
-
 export class AppComponent {
-    pageTitle: any = 'The Great Walleye Store'
+  pageTitle: string = 'The Great Walleye Store';
 }
