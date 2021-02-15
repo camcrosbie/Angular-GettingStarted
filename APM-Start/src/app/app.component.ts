d0e526b02cd7aae55c7b93c9930660a46f5cc479
Cam Crosbie <cam@camcrosbie.com>
2021-02-15 17:26:49 -0500

    updated templateURL in AppComponent class AB#6

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'walleye-root',
-  templateUrl: 'app.component.html',
+  templateUrl: './app.component.html',
 })
 export class AppComponent {
   pageTitle: string = 'The Great Walleye Store';
