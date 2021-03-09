154d55d6d1fdd7bb0c040333767d098eab77f278
Cam Crosbie <cam@camcrosbie.com>
2021-03-08 19:33:15 -0500

    AB#7 updated packages and angular in APM-Final

--- APM-Final/src/app/app.module.ts
+++ APM-Final/src/app/app.module.ts
@@ -16,10 +16,10 @@ import { ProductModule } from './products/product.module';
     BrowserModule,
     HttpClientModule,
     RouterModule.forRoot([
-      { path: 'welcome', component: WelcomeComponent },
-      { path: '', redirectTo: 'welcome', pathMatch: 'full' },
-      { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
-    ]),
+    { path: 'welcome', component: WelcomeComponent },
+    { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+    { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
+], { relativeLinkResolution: 'legacy' }),
     ProductModule
   ],
   bootstrap: [AppComponent]
